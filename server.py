520bd8f9a66d5d7a6ba556f91f7f7422401c5a54
ocat1 <owen.eerdmans@me.com>
2025-11-22 18:23:59 -0600

    Add initial html file

---
+++ server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+
+os.chdir(os.path.join(os.path.dirname(__file__), 'static'))
+
+PORT = 5000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
+    print(f"Server running on http://0.0.0.0:{PORT}")
+    httpd.serve_forever()
