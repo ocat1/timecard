1286a37f8e7d8f74c843610046adfe0c3ac5dd04
Owen Eerdmans <owen.eerdmans@garmin.com>
2025-11-24 14:22:18 -0600

    Bug fixes
    Change-Id: I458b4783f2ba41fac69110e4460be4887e30ef0d

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import http.server
 import socketserver
 import os
 
-os.chdir(os.path.join(os.path.dirname(__file__), 'static'))
+os.chdir(os.path.dirname(__file__))
 
 PORT = 5000
 Handler = http.server.SimpleHTTPRequestHandler
